bf285bac1b090f56c4066ab48bcd096bbb9d0e70
siddharthsingla <37585197+siddharthsingla@users.noreply.github.com>
2023-07-26 18:55:59 -0500

    Update 05-01-peering-demo-buildout.ps1

--- 05-Design-and-Implement-Cross-VNet-Connectivity/05-01-peering-demo-buildout.ps1
+++ 05-Design-and-Implement-Cross-VNet-Connectivity/05-01-peering-demo-buildout.ps1
@@ -40,11 +40,11 @@ az network vnet create --name cake-spoke2-vnet --resource-group $rg --location $
 
 # Create three Linux machines. One in each network
 
-az vm create --resource-group $rg --name spoke-1-vm --image UbuntuLTS --generate-ssh-keys --public-ip-address myPublicIP-spoke-1-vm --public-ip-sku Standard --vnet-name cake-spoke1-vnet --subnet spoke-1-subnet-a --size Standard_B1ls --storage-sku Standard_LRS --no-wait
+az vm create --resource-group $rg --name spoke-1-vm --image UbuntuLTS --generate-ssh-keys --public-ip-address myPublicIP-spoke-1-vm --public-ip-sku Standard --vnet-name cake-spoke1-vnet --subnet spoke-1-subnet-a --size Standard_B1s --storage-sku Standard_LRS --no-wait
 
-az vm create --resource-group $rg --name spoke-2-vm --image UbuntuLTS --generate-ssh-keys --public-ip-address myPublicIP-spoke-2-vm --public-ip-sku Standard --vnet-name cake-spoke2-vnet --subnet spoke-2-subnet-a --size Standard_B1ls --storage-sku Standard_LRS --no-wait
+az vm create --resource-group $rg --name spoke-2-vm --image UbuntuLTS --generate-ssh-keys --public-ip-address myPublicIP-spoke-2-vm --public-ip-sku Standard --vnet-name cake-spoke2-vnet --subnet spoke-2-subnet-a --size Standard_B1s --storage-sku Standard_LRS --no-wait
 
-az vm create --resource-group $rg --name hub-nva-vm --image UbuntuLTS --generate-ssh-keys --public-ip-address myPublicIP-nva --public-ip-sku Standard --vnet-name cake-hub-vnet --subnet nva-subnet --size Standard_B1ls --storage-sku Standard_LRS
+az vm create --resource-group $rg --name hub-nva-vm --image UbuntuLTS --generate-ssh-keys --public-ip-address myPublicIP-nva --public-ip-sku Standard --vnet-name cake-hub-vnet --subnet nva-subnet --size Standard_B1s --storage-sku Standard_LRS
 
 # Update the NVA VM to enable IP forwarding. This needs to be enabled on both the VM NIC and within the OS
 # via extension.
